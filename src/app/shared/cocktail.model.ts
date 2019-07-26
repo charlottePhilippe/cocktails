3e421120a976c779b00bf76834df177d48042742
cphilippe <cphilippe@ippon.fr>
2019-07-26 16:58:08 +0200

    binding entre composants avec input

--- src/app/shared/cocktail.model.ts
+++ src/app/shared/cocktail.model.ts
@@ -1,8 +1,12 @@
 export class Cocktail {
     public name: string;
+    public img: string;
+    public desc: string;
 
-    constructor(name: string) {
+    constructor(name: string, img: string, desc: string) {
         this.name = name;
+        this.img = img;
+        this.desc =  desc;
          
     }
 
